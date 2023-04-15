5c89d6faaaa73e536cdd47cc5f21198440dd8f15
VeronikaKan <kan.v98@gmail.com>
2023-04-16 00:33:09 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,171 @@
+// 1 задание
+
+// let a = prompt("Vvedite imya")
+//  if(a === "10"){
+//     console.log("true")
+// } else {
+//     console.log("false")
+// }
+
+// 2задание
+
+
+// let a = 50
+// let b = 100
+// if(a > b){
+//     console.log("a > b")
+// }else {
+//     console.log("a < b")
+// }
+
+// 3задание
+
+// let a = -2
+// if(a > 0){
+//     console.log("positive")
+// }else if (a === 0){
+//     console.log("ravno")
+// }else{
+//     console.log("negative")
+// }
+
+// 4задание
+
+// let num = 46
+// if (num%2 === 0){
+//     console.log("chetnoe")
+// }else{
+//     console.log("nechet")
+// }
+
+// 5задание
+
+// let a = 10
+// let b = 2
+// if(a/b%2 === 0){
+//     console.log("chetnoe")
+// }else{
+//     console.log("nechetnoe")
+// }
+
+// 6задание
+
+// let s = true
+// console.log(typeof s);
+// let s = 234
+// console.log(typeof s);
+// let s = "Veronika"
+// console.log(typeof s);
+
+// 7задание
+
+// let a = 3
+// if (a > 1 && a < 9){
+//     console.log("verno")
+// }else{
+//     console.log("neverno")
+// }
+
+// 8задание
+
+// let a = 2
+// if(a === 3 || a ===7)
+// {console.log(a + 7)
+// }
+// else{
+//    console.log(a / 10);
+// }
+
+// 9задание
+
+// let a = 4
+// let b = 3
+// if( a <= 0 && b >= 5){
+//     console.log(a + b);
+// }else{
+//     console.log(a - b);
+// }
+
+// 10задание
+
+// let a = 3
+// let b = 5
+// if((a > 4 && a < 10 )||( b >= 7 && b < 17)){
+//     console.log("verno");
+// }else{
+//     console.log("neverno");
+// }
+
+// 11задание
+
+// let month = 1
+// if( month <=2 || month === 12)
+// {
+//     console.log("zima")
+// }else if(month >= 3 && month <= 5)
+// {
+// console.log("vesna")
+// }else if(month >= 6 && month <= 8)
+// {
+//     console.log("leto");
+// }else if (month >= 9 && month <= 11){
+//     console.log("osen");
+// }else{
+//     console.log("takogo mesyaca net");
+// }
+
+// 12задание
+
+
+// let day = 4
+// if(day >= 1 && day <= 10){
+//     console.log("pervaya");
+// } else if(day >= 11 && day <=20){
+//     console.log("vtoraya");
+// }else if( day >= 21 && day <=31){
+//     console.log("tretya");
+// }else{
+//     console.log("net takogo dnya");
+// }
+
+// 13задание
+
+
+// let a = "12345"
+// if( a[0] ==="1" || a[0] === "2" || a[0] === "3" ){
+//     console.log("da");
+// }else{
+//     console.log("net");
+// }
+
+
+// 14задание
+
+// let age = "14"
+
+// if(age[1] === "1" && age !== "11" ){
+//     console.log( `Мне ${age} год`)
+// } else if((2<= age[1] && age[1] <= 4) && age[0] !== '1'){
+//     console.log(`Мне ${age} года`);
+// }else{
+//     console.log( `Мне ${age} лет`);
+// }
+
+
+// 15 задание
+
+
+// let time = 60
+// if(time >= 0 & time <=14){
+//     console.log("pervaya chetvert");
+// } else if(time >= 15 & time <= 29){
+//     console.log("vtoraya chetvert");
+// } else if(time >= 30 & time <= 44){
+//     console.log("tretya chetvert");
+// } else if(time >= 45 & time <= 59){
+//     console.log("chetvertaya chetvert");
+// } else {
+//     console.log("nevernoe chislo");
+// }
+
+
